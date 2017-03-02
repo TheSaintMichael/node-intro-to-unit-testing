fe7f0eda69bb3b4f21fb051c9f1c46cd15f638c9
TheSaintMichael <MichaelQuiban@outlook.com>
2017-03-01 23:58:48 -0800

    Readability changes

--- adder.js
+++ adder.js
@@ -1,9 +1,9 @@
-function adder(a, b) {
+function adder (a, b) {
   // validate that both inputs are numbers
   if (!(typeof a === 'number' && typeof b === 'number')) {
-    throw Error('Oh no!');
+    throw Error('Oh no!')
   }
-  return a + b;
+  return a + b
 }
 
-module.exports = adder;
+module.exports = adder
